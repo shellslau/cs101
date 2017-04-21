2b98160e8c66af7494f1c453991848183d027080
Michelle Slaughter <michelleslaughter@eduroam-169-233-250-56.ucsc.edu>
2017-04-20 22:04:54 -0700

    added graph01.c

---
+++ pa1/graph01.c
@@ -0,0 +1,10 @@
+/* Michelle Slaughter
+ * mislaugh 
+*/
+#include "intVec.c"
+
+int main(int argc, char* argv[]){
+    if (argv[0] == null){
+        fprintf(stderr, "%s\n", "Error: cannot execute with no input or output");
+    }
+}
